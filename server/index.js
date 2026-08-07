089146122588d10a9d7b09adf10578e2f67ae991
Escort VIP Developer <dev@escortvip.com>
2026-08-07 05:20:20 -0400

    Increase Express payload limit to 50mb and fix safely parse HTML error responses

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Serve static uploads
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
